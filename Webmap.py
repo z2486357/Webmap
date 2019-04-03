9f544a6d5d2f1a761d809858e77816cd3604c6e2
z2486357 <z2486357@gmail.com>
2019-04-03 23:52:10 +0800

    update - finish the marker

--- Webmap.py
+++ Webmap.py
@@ -1,6 +1,38 @@
 import folium
+import pandas
 
-map=folium.Map(location=[38.58,-99.09], zoom_start=6)
-for i in [[38.59,-99.15],[38.25,-99.35]]:
-    map.add_child(folium.Marker(location=i,popup="I am a marker!!!", icon=folium.Icon(color="red", icon_color="green")))
+data=pandas.read_csv("Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+name=list(data["NAME"])
+location=list(data["LOCATION"])
+status=list(data["STATUS"])
+elev=list(data["ELEV"])
+
+def decide_color(elev):
+    if elev < 1000:
+        return "green"
+    elif 1000 <= elev < 3000:
+        return "orange"
+    else:
+        return "red"
+
+lat_total=0
+for i in lat:
+    lat_total=lat_total+i
+lat_average=lat_total/len(lat)
+
+lon_total=0
+for i in lon:
+    lon_total=lon_total+i
+lon_average=lon_total/len(lon)
+
+map=folium.Map(location=[lat_average,lon_average], zoom_start=7, tiles="Mapbox Bright")
+
+for i in range(0,len(data)):
+    map.add_child(folium.Marker(location=[lat[i],lon[i]],popup=str(elev[i])+" m ", icon=folium.Icon(color=decide_color(elev[i]), icon_color="white")))
+"""
+folium.CircleMarker for circle marker
+"""
 map.save("Map1.html")
+
